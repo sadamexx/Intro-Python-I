5e3d04b113db0a8af100df0bf754935b189f701b
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-07-06 20:40:27 -0700

    working on slices

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,18 @@
 # Print "Hello, world!" to your terminal
+print("Hello World")
+
+#or another way
+
+message = "Hello World"
+print(message)
+
+#another way
+
+name = "Earth"
+print(f"Hello {name}")
+
+#another way
+if name == "World":
+    print(f"Hello {name}")
+else:
+    print(f"Hello {name}, it's nice to meet you")
